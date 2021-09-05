e1d89ee65bfd712d0bc887c11d1dead97da629e5
mikoloism <mikoloism.github@gmail.com>
2021-09-05 23:39:31 +0430

    [ ✅ ] check some todo's [ 🔨 ] fix some bugs [ ♻️ ] rename `MUSIC_LIST` array to `trackList` [ 🗑️ ] remove `MusicPlayer` class and that object's

--- main.js
+++ main.js
@@ -12,144 +12,101 @@ const $_cover = query('#music-cover');
 const $_trackName = query('#music-title');
 const $_artist = query('#music-desc');
 const $audio = query('#music-audio');
-
 const defaultTrack = {
   // id: (() => trackList.slice(-1).id + 1)(),
   cover: `https://www.iphonefaq.org/files/styles/large/public/apple_music.jpg?itok=nqYGxWgh`,
   title: `Unknown`,
   artist: `unknown`,
 };
-
 const fixPath = `https://raw.githubusercontent.com/miko-github/miko-github/gh_assets/assets/sounds`;
-// TODO : rename to `trackList` or `playList`
 // TODO : use online src link
-// TODO : `id` start from 0
 // NOTE : artist can be array of artist [a, b, ...] also
-let MUSIC_LIST = [
+let trackList = [
   {
-    id: 1,
+    id: 0,
     title: `Arayeshe Ghaliz`,
     src: `${fixPath}/Homayoun%20Shajarian%20-%20Arayeshe%20Ghaliz.mp3`,
     artist: `Homayoun Shajarian`,
     cover: undefined,
   },
   {
-    id: 2,
+    id: 1,
     title: `Norouz`,
     src: `${fixPath}/Homayoun%20Shajarian%20&%20Sohrab%20Pournazeri%20-%20Norouz.mp3`,
     artist: `Homayoun Shajarian`,
     cover: undefined,
   },
   {
-    id: 3,
+    id: 2,
     title: `Shole Var`,
     src: `${fixPath}/Homayoun_shajarian_SholehVar_Final.mp3`,
     artist: `Homayoun Shajarian`,
     cover: undefined,
   },
   {
-    id: 4,
+    id: 3,
     title: `Saghi Bia`,
     src: `${fixPath}/MohammadReza%20Shajaryan%20-%20Saghi%20Bia.mp3`,
     artist: `Mohammad Reza Shajarian`,
     cover: undefined,
   },
   {
-    id: 5,
+    id: 4,
     title: `Rap God`,
     src: `${fixPath}/Eminem%20-%20Rap%20God.mp3`,
     artist: `Eminem`,
     cover: undefined,
   },
   {
-    id: 6,
+    id: 5,
     title: `Bande Naaf`,
     src: `${fixPath}/yas-bande-naaf-ta-khatte-saaf-ft-moer.mp3`,
     artist: `Yas`,
     cover: undefined,
   },
   {
-    id: 7,
+    id: 6,
     title: `Halal Osoun`,
     src: `${fixPath}/ali_ardavan%20&%20sohrab%20mj_halal_osoun.mp3`,
     artist: [`Ali Ardavan`, `Sohrab MJ`],
     cover: undefined,
   },
   {
-    id: 8,
+    id: 7,
     title: `Sobhoone`,
     src: `${fixPath}/Ho3ein%20-%20Sobhoone.mp3`,
     artist: `Ho3ein`,
     cover: undefined,
   },
   {
-    id: 9,
+    id: 8,
     title: `Makhlase Kaloom`,
     src: `${fixPath}/Shayea%20-%20Makhlase%20Kaloom.mp3`,
     artist: `Shayea`,
     cover: undefined,
   },
   {
-    id: 10,
+    id: 9,
     title: `Tukur Tukur`,
     src: `${fixPath}/Tukur%20Tukur%20-%20Arijit%20Singh.mp3`,
     artist: undefined,
     cover: undefined,
   },
   {
-    id: 11,
+    id: 10,
     title: `Tharki Chokro`,
     src: `${fixPath}/01%20-%20Tharki%20Chokro.mp3`,
     artist: undefined,
     cover: undefined,
   },
   {
-    id: 12,
+    id: 11,
     title: `Vilayati Sharaab`,
     src: `${fixPath}/Vilayati.Sharaab.mp3`,
     artist: [`Darshan Raval`, `Neeti Mohan`],
     cover: undefined,
   },
 ];
-
-// TODO : make every thing with below class
-class MusicPlayer {
-  constructor(player, trackList = []) {
-    this.trackList = trackList;
-    this.player = player;
-    this.current = 0;
-    this.currentTrack = this.trackList[this.current];
-  }
-  play() {
-    this.currentTrack = this.trackList[this.current];
-    let { src } = this.currentTrack;
-    this.player.setAttribute('src', src);
-    this.player.play();
-    return src;
-  }
-  pause() {
-    this.player.pause();
-  }
-  forward() {
-    if (this.current + 1 > this.trackList.length - 1) {
-      this.current = 0;
-    } else {
-      this.current += 1;
-    }
-    this.play();
-  }
-  backward() {
-    if (this.current - 1 < 0) {
-      this.current = this.trackList.length - 1;
-    } else {
-      this.current -= 1;
-    }
-    this.play();
-  }
-  duration() {}
-}
-
-const $player = new MusicPlayer($audio, MUSIC_LIST);
 
 // +++ Event Handler +++ //
 function fixArtist(artist) {
@@ -158,24 +115,24 @@ function fixArtist(artist) {
 }
 function goForward() {
   let currentTrack = selectCurrentTrack();
-  if (currentTrack.id + 1 > MUSIC_LIST.length)
-    changeMetaData(MUSIC_LIST[0].src);
-  else changeMetaData(MUSIC_LIST[currentTrack.id].src);
+  if (currentTrack.id + 1 > trackList.length - 1)
+    changeMetaData(trackList[0].src);
+  else changeMetaData(trackList[currentTrack.id + 1].src);
   $audio.play();
 }
 function goBackward() {
   let currentTrack = selectCurrentTrack();
-  if (currentTrack.id - 1 <= 0)
-    changeMetaData(MUSIC_LIST[MUSIC_LIST.length - 1].src);
-  else changeMetaData(MUSIC_LIST[currentTrack.id - 2].src);
+  if (currentTrack.id - 1 < 0)
+    changeMetaData(trackList[trackList.length - 1].src);
+  else changeMetaData(trackList[currentTrack.id - 1].src);
   $audio.play();
 }
 
 function selectCurrentTrack() {
-  return MUSIC_LIST.filter((track) => track.src == $audio.src)[0];
+  return trackList.filter((track) => track.src == $audio.src)[0];
 }
 function changeMetaData(src) {
-  let currentTrack = selectCurrentTrack() || $player.currentTrack;
+  let currentTrack = selectCurrentTrack();
   $_trackName.textContent = currentTrack.title || defaultTrack.title;
   $_artist.textContent = fixArtist(currentTrack.artist) || defaultTrack.artist;
   $_cover.setAttribute('src', currentTrack.cover || defaultTrack.cover);
@@ -186,8 +143,8 @@ listener($audio, 'durationchange', () => {
   let currentTrack = selectCurrentTrack();
   // fixBase64(currentTrack.src, (file) => {
   //   fetchMetadata(file, (tags) => {
-  //     MUSIC_LIST.splice(currentTrack.id - 1, 1, {
-  //       id: (() => MUSIC_LIST[MUSIC_LIST.length - 1].id + 1)(),
+  //     trackList.splice(currentTrack.id - 1, 1, {
+  //       id: (() => trackList[trackList.length - 1].id + 1)(),
   //       src: currentTrack.src,
   //       title: tags.title,
   //       artist: tags.artist,
@@ -208,8 +165,8 @@ listener($_file, 'change', function ({ target }) {
     let file = target.files[index];
     let src = URL.createObjectURL(file);
     metadata(file, ({ tags }) => {
-      MUSIC_LIST.push({
-        id: (() => MUSIC_LIST[MUSIC_LIST.length - 1].id + 1)(),
+      trackList.push({
+        id: (() => trackList[trackList.length - 1].id + 1)(),
         title: tags.title,
         cover: tags.picture && fetchCover(tags.picture),
         artist: tags.artist,
@@ -219,8 +176,8 @@ listener($_file, 'change', function ({ target }) {
     // ALT
     /**
     fetchMetadata(file, (tags) => {
-      MUSIC_LIST.push({
-        id: (() => MUSIC_LIST[MUSIC_LIST.length - 1].id + 1)(),
+      trackList.push({
+        id: (() => trackList[trackList.length - 1].id + 1)(),
         title: tags.title,
         cover: tags.v2.APIC[0] && fetchCover2(tags.v2.APIC[0]),
         artist: tags.v2.TPE1,
@@ -303,22 +260,13 @@ listener($_seek, 'click', (ev) => {
 });
 
 // [backward]:click
-listener($_backward, 'click', () =>
-  // ALT : $player.backward();
-  goBackward()
-);
+listener($_backward, 'click', () => goBackward());
 
 // [forward]:click
-listener($_forward, 'click', () =>
-  // ALT : $player.forward();
-  goForward()
-);
+listener($_forward, 'click', () => goForward());
 
 // NOTE : when sound finished, then, play next music
-listener($audio, 'ended', () =>
-  // ALT : $player.forward();
-  goForward()
-);
+listener($audio, 'ended', () => goForward());
 
 // [play]:click
 listener($_play, 'click', () =>
